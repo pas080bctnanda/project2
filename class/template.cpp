79c4a38eb2ada8ff48c85341dae3fe86bf32b7b5
Aayush <pas080bct003@wrc.edu.np>
2024-07-30 09:10:54 +0545

    day2

---
+++ class/template.cpp
@@ -0,0 +1,25 @@
+//DRY
+// Generic programming 
+#include<iostream>
+#include<vector>
+using namespcae std ;
+template<class T>
+class vector {
+    T * arr ;
+    public :
+    vector(T*ar)
+}
+
+class vector 
+{
+ int * arr ;
+ int size ;
+ public :
+
+};
+
+
+int main()
+{
+  return 0 ;  
+}
--- project2.cpp
+++ project2.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 #include<fstream>
+#include<string.h>
+#include<algorithm>
 using namespace std ;
+
+
+class temp 
+{
+    private:
+    string id , name , author ;
+    fstream file ;
+    public :
+    void addbook();
+    void showall();
+    void extractbook();
+
+};
 
 
 int main()
@@ -26,6 +41,65 @@ int main()
 
          case '4 ':
         break ;
+        
+        default:
+        cout<< " invalid choice "<< endl ;
     }
     return 0 ;
+}
+
+void temp::addbook()
+{
+    cout<< " enter boook id of the "<< endl ;
+    getline(cin , id );
+    cout << " enter the name of the book "<< endl ;
+    getline(cin , name );
+    cout<< " enter the authors name "<< endl ;
+    getline(cin , author );
+
+    file.open("bookData.txt", ios::out | ios :: app);
+    file<<id << "*" << name << "*"<<author << endl ;
+    file.close();
+
+}
+
+void temp :: showall()
+{
+    file.open(" bookData,txt", ios::in);
+    getline(file,id,"*");
+    getline(file,name ,"*");
+    getline(file,author,"*");
+        cout<<"\n\n";
+        cout<<"Book id \t\t Book name \t\t Authors name "<<endl ;    
+    while(!file.eof())
+    {
+        cout<< id <<" \t\t\t"<< name << "\t\t\t"<< author<<endl ;
+
+    }
+    file.close();
+}
+
+void temp :: extractbook()
+{
+    showall();
+    cout<< " enter the id of the book you want to search  "<< endl ;
+    getline(cin, search);
+    file.open("bookData.txt", ios::in);
+    getline(file , id , "*");
+    getline(file , name , "*");
+    getline(file , author , "*");
+     cout<<"\n\n";
+        cout<<"Book id \t\t Book name \t\t Authors name "<<endl ;   
+     while(!file.eof())
+     {
+        if(search-- id )
+        {
+         cout<< id <<" \t\t\t"<< name << "\t\t\t"<< author<<endl ;
+         cout<<" Book extracted sucefully"<< endl ;
+        } 
+    getline(file , id , "*");
+    getline(file , name , "*");
+    getline(file , author , "*");
+     }
+
 }
